5e86bf97006337ae22912e12d790625e76911fd7
jongrom2 <jongrom2@nate.com>
2024-09-14 00:21:30 +0900

    p189까지 완료.
    Signed-off-by: jongrom2 <jongrom2@nate.com>

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  //const 생성자
+  const HomeScreen({Key? key}) : super(key: key);
+//p188
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Text('Home Screen'),
+    );
+  }
+}
